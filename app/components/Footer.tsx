21b58a1af6772d1c35f30743ffcf86ccd749172d
scriptGenie <scriptGenie@protonmail.com>
2024-01-04 04:37:55 -0500

    Updated footer to 2024

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -10,7 +10,7 @@ const Footer = () => {
   return (
       <footer className="footer fixed bottom-0 left-0 right-0  px-2 z-50 flex justify-center  bg-slate-800 text-white ">
         <aside className="items-center grid-flow-col">
-          <p>Made By Akash Singh © 2023 - All rights reserved</p>
+          <p>Made By Akash Singh © 2024 - All rights reserved</p>
         </aside>
         <nav className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
           <IconButton
