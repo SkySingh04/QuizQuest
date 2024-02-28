8c4f98e15a6adaad7ae7b878fb60f4a2c5aad732
Areeb Ahmed <areebahmed0709@gmail.com>
2024-02-29 00:37:10 +0530

    FIXED:#2 Added a Quiz Lock Feature

--- app/student/page.tsx
+++ app/student/page.tsx
@@ -7,26 +7,25 @@ import { db } from '../firebase';
 import { onAuthStateChanged } from 'firebase/auth';
 import { useRouter } from 'next/navigation';
 
-// Define a function to fetch quiz data
 async function fetchQuizData() {
-  const quizCollection = collection(db, 'quizzes');
-  const querySnapshot = await getDocs(quizCollection);
-  const quizData:any = [];
+  const querySnapshot = await getDocs(collection(db, 'quizzes'));
+  const quizData: any = [];
   querySnapshot.forEach((doc) => {
-    quizData.push(doc.data());
+    if (!doc.data().data.isDeleted && !doc.data().data.isLocked) {
+      quizData.push(doc.data());
+    }
   });
-  // console.log(quizData);
   return quizData;
 }
 
 async function fetchUserData() {
   const userCollection = collection(db, 'users');
   const querySnapshot = await getDocs(userCollection);
-  const userData:any = [];
+  const userData: any = [];
   querySnapshot.forEach((doc) => {
     userData.push(doc.data());
   });
-  const currentUserData = userData.filter((entry:any) => {
+  const currentUserData = userData.filter((entry: any) => {
     return (
       entry.uid == auth.currentUser?.uid
     );
@@ -34,25 +33,25 @@ async function fetchUserData() {
   console.log(currentUserData);
   return userData;
 }
+
 // StudentPage component
 const StudentPage = () => {
-    const router = useRouter();
+  const router = useRouter();
 
-    const [user, setUser] = useState(auth.currentUser);
-    const [userData, setUserData] = useState([]);
+  const [user, setUser] = useState(auth.currentUser);
+  const [userData, setUserData] = useState([]);
 
-    useEffect(() => {
-        // Check the user's authentication state
-        onAuthStateChanged(auth, (user) => {
-          if (user) {
-            
-            setUser(user as any);
-          } else {
-            // Redirect unauthenticated users to the login page
-            router.push('/login');
-          }
-        });
-      }, []);
+  useEffect(() => {
+    // Check the user's authentication state
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user as any);
+      } else {
+        // Redirect unauthenticated users to the login page
+        router.push('/login');
+      }
+    });
+  }, []);
 
   const [quizzes, setQuizzes] = useState([]);
 
@@ -66,60 +65,51 @@ const StudentPage = () => {
     }
     fetchData();
   }, []);
-  // console.log("quizzes are " + quizzes)
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       {/* Quiz Cards Section */}
       <div className="mt-8 text-gray-300 text-center">
-  <h1 className="text-3xl font-semibold mb-6">Join the quiz challenge and unlock your potential!</h1>
-  <div className="flex flex-wrap justify-center mt-4">
-  {quizzes.map((quiz: any) => {
-  const userAttempts: any = userData.find(
-    (data: any) => data.uid === auth.currentUser?.uid
-  );
+        <h1 className="text-3xl font-semibold mb-6">Join the quiz challenge and unlock your potential!</h1>
+        <div className="flex flex-wrap justify-center mt-4">
+          {quizzes.map((quiz: any) => {
+            const userAttempts: any = userData.find(
+              (data: any) => data.uid === auth.currentUser?.uid
+            );
 
-  const isQuizAttempted =
-    userAttempts &&
-    userAttempts.quizData.some((attempt: any) => attempt.quizId === quiz.data.id);
+            const isQuizAttempted =
+              userAttempts &&
+              userAttempts.quizData.some((attempt: any) => attempt.quizId === quiz.data.id);
 
-  return (
-    <div
-      key={quiz.data.id}
-      className={`bg-gradient-to-r from-customBlue to-customViolet font-black px-6 py-4 rounded-md m-4 cursor-pointer transform hover:scale-105 transition duration-300 ${
-        isQuizAttempted ? 'opacity-50' : ''
-      }`}
-    >
-      {isQuizAttempted ? (
-        <div>
-          <p className="text-lg">Quiz Name: {quiz.data.quizName}</p>
-          <p>Course: {quiz.data.course}</p>
-          <p>Course Code: {quiz.data.courseCode}</p>
-          <p>This quiz has been attempted.</p>
+            return (
+              <div
+                key={quiz.data.id}
+                className={`bg-gradient-to-r from-customBlue to-customViolet font-black px-6 py-4 rounded-md m-4 cursor-pointer transform hover:scale-105 transition duration-300 ${isQuizAttempted ? 'opacity-50' : ''
+                  }`}
+              >
+                {isQuizAttempted ? (
+                  <div>
+                    <p className="text-lg">Quiz Name: {quiz.data.quizName}</p>
+                    <p>Course: {quiz.data.course}</p>
+                    <p>Course Code: {quiz.data.courseCode}</p>
+                    <p>This quiz has been attempted.</p>
+                  </div>
+                ) : (
+                  <Link
+                    href={`/quiz?id=${quiz.data.id}&name=${quiz.data.quizName}&course=${quiz.data.course}&coursecode=${quiz.data.courseCode}`}
+                  >
+                    <div>
+                      <p className="text-lg">Quiz Name: {quiz.data.quizName}</p>
+                      <p>Course: {quiz.data.course}</p>
+                      <p>Course Code: {quiz.data.courseCode}</p>
+                    </div>
+                  </Link>
+                )}
+              </div>
+            );
+          })}
         </div>
-      ) : (
-        <Link
-          href={`/quiz?id=${quiz.data.id}&name=${quiz.data.quizName}&course=${quiz.data.course}&coursecode=${quiz.data.courseCode}`}
-        >
-          <div>
-            <p className="text-lg">Quiz Name: {quiz.data.quizName}</p>
-            <p>Course: {quiz.data.course}</p>
-            <p>Course Code: {quiz.data.courseCode}</p>
-          </div>
-        </Link>
-      )}
-    </div>
-  );
-})}
-
-
-
-
-
-
-  </div>
-</div>
-
+      </div>
     </div>
   );
 };
